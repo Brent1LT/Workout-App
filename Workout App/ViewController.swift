da1c3fbc343c01aa280e05031525f394ac1c0e59
Brent Bumann <brent.bumann@aol.com>
2019-09-02 14:37:10 -0700

    Initial commit

---
+++ Workout App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workout App
+//
+//  Created by Brent Bumann on 9/2/19.
+//  Copyright © 2019 Brent Bumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
